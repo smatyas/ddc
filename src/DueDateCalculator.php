3da57ca764519b58e0d522b97b213adee91b919c
Mátyás Somfai <somfai.matyas@gmail.com>
2017-08-15 22:26:41 +0200

    implemented a not so effective naive solution using a recursive function and added tests

--- src/DueDateCalculator.php
+++ src/DueDateCalculator.php
@@ -21,26 +21,67 @@ class DueDateCalculator
      *
      * @param \DateTime $submitDate The submit date.
      * @param int $turnaroundTime The turnaround time.
+     * @return \DateTime The due date.
      */
     public static function calculateDueDate(\DateTime $submitDate, $turnaroundTime)
     {
-        if (!is_int($turnaroundTime) || $turnaroundTime < 0) {
+        if (!is_int($turnaroundTime) || $turnaroundTime <= 0) {
             throw new \InvalidArgumentException('The turnaroundTime parameter must be a positive integer.');
         }
 
-        $dayOfWeek = $submitDate->format('N');
-        if ($dayOfWeek > 5) {
+        if (!self::isWorkingHour($submitDate)) {
             throw new \InvalidArgumentException('The submitDate parameter must be a workday between 9AM and 5PM.');
         }
 
-        $minTime = clone $submitDate;
+        $dueDate = self::incrementDueDate(clone $submitDate, $turnaroundTime);
+
+        return $dueDate;
+    }
+
+    /**
+     * Tells if the given date time is in a working hours interval.
+     *
+     * @param \DateTime $dateTime
+     * @return bool
+     */
+    private static function isWorkingHour(\DateTime $dateTime)
+    {
+        $dayOfWeek = (int) $dateTime->format('N');
+        if ($dayOfWeek > 5) {
+            return false;
+        }
+
+        $minTime = clone $dateTime;
         $minTime->setTime(static::WORK_HOURS_START, 0);
 
-        $maxTime = clone $submitDate;
+        $maxTime = clone $dateTime;
         $maxTime->setTime(static::WORK_HOURS_END, 0);
 
-        if ($submitDate < $minTime || $submitDate > $maxTime) {
-            throw new \InvalidArgumentException('The submitDate parameter must be a workday between 9AM and 5PM.');
+        if ($dateTime < $minTime || $dateTime >= $maxTime) {
+            return false;
         }
+
+        return true;
+    }
+
+    /**
+     * A recursive function that increments the due date until the turnaround time is not 0.
+     *
+     * @param \DateTime $dueDate
+     * @param $turnaroundTime
+     * @return \DateTime
+     */
+    private static function incrementDueDate(\DateTime $dueDate, $turnaroundTime)
+    {
+        if ($turnaroundTime === 0) {
+            return $dueDate;
+        }
+
+        $dueDate->modify('+1 hour');
+        if (self::isWorkingHour($dueDate)) {
+            $turnaroundTime--;
+        }
+
+        return self::incrementDueDate($dueDate, $turnaroundTime);
     }
 }
--- tests/DueDateCalculatorTest.php
+++ tests/DueDateCalculatorTest.php
@@ -19,7 +19,8 @@ class DueDateCalculatorTest extends TestCase
             [null],
             ['1'],
             [1.2],
-            [[]]
+            [[]],
+            [0],
         ];
     }
 
@@ -53,5 +54,32 @@ class DueDateCalculatorTest extends TestCase
     {
         $submitDate = new \DateTime($submitDateString);
         DueDateCalculator::calculateDueDate($submitDate, 1);
+    }
+
+    public function calculateDueDateDataProvider()
+    {
+        return [
+            ['2017-08-15T10:11:12', 2, '2017-08-15T12:11:12'],
+            ['2017-08-15T16:00:00', 1, '2017-08-16T9:00:00'],
+            ['2017-08-15T16:30:00', 1, '2017-08-16T9:30:00'],
+            ['2017-08-15T16:30:00', 2, '2017-08-16T10:30:00'],
+            ['2017-08-15T9:00:00', 8, '2017-08-16T9:00:00'],
+            ['2017-08-15T9:00:00', 16, '2017-08-17T9:00:00'],
+            ['2017-08-18T12:00:00', 8, '2017-08-21T12:00:00'],
+            ['2017-08-18T12:00:00', 16, '2017-08-22T12:00:00'],
+            ['2017-08-18T12:00:00', 40, '2017-08-25T12:00:00'],
+            ['2017-08-18T12:00:00', 47, '2017-08-28T11:00:00'],
+        ];
+    }
+
+    /**
+     * @dataProvider calculateDueDateDataProvider
+     */
+    public function testCalculateDueDate($submitDateString, $turnaroundTime, $expectedDateString)
+    {
+        $submitDate = new \DateTime($submitDateString);
+        $expectedDate = new \DateTime($expectedDateString);
+        $actualDate = DueDateCalculator::calculateDueDate($submitDate, $turnaroundTime);
+        $this->assertEquals($expectedDate, $actualDate);
     }
 }
