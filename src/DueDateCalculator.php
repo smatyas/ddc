fc3b3cf4b5f48386808e38cc7f275997170e0b78
Mátyás Somfai <somfai.matyas@gmail.com>
2017-08-15 19:14:09 +0200

    added tests for the turnaround time parameter

--- src/DueDateCalculator.php
+++ src/DueDateCalculator.php
@@ -13,5 +13,16 @@ namespace Smatyas\Ddc;
  */
 class DueDateCalculator
 {
-
+    /**
+     * Calculates the due date for the given submit date and turnaround time.
+     *
+     * @param \DateTime $submitDate The submit date.
+     * @param int $turnaroundTime The turnaround time.
+     */
+    public static function calculateDueDate(\DateTime $submitDate, $turnaroundTime)
+    {
+        if (!is_int($turnaroundTime) || $turnaroundTime < 0) {
+            throw new \InvalidArgumentException('The turnaroundTime parameter must be a positive integer.');
+        }
+    }
 }
--- tests/DueDateCalculatorTest.php
+++ tests/DueDateCalculatorTest.php
@@ -13,8 +13,24 @@ use PHPUnit\Framework\TestCase;
 
 class DueDateCalculatorTest extends TestCase
 {
-    public function testPlaceholder()
+    public function invalidTurnaroundTimeDataProvider()
     {
-        $this->markTestIncomplete('placeholder');
+        return [
+            [null],
+            ['1'],
+            [1.2],
+            [[]]
+        ];
+    }
+
+    /**
+     * @dataProvider invalidTurnaroundTimeDataProvider
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage The turnaroundTime parameter must be a positive integer.
+     */
+    public function testInvalidTurnaroundTime($turnaroudTime)
+    {
+        $submitDate = new \DateTime();
+        DueDateCalculator::calculateDueDate($submitDate, $turnaroudTime);
     }
 }
